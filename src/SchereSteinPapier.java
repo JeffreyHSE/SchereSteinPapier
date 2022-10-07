e2f1517a3eda9cc454c9f3c2e57636b1b22dc866
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-07 13:21:09 +0200

    followed code convention that I got thought.

--- src/SchereSteinPapier.java
+++ src/SchereSteinPapier.java
@@ -3,28 +3,15 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class SchereSteinPapier {
-    public static String userInput;
-    public static String computerOutput;
+    private String userInput;
+    private String computerOutput;
+    private Scanner ssp;
+    private ArrayList<String> SchereSteinPapierArray = new ArrayList<>();
 
-    public static Scanner ssp;
-
-    public static ArrayList<String> SchereSteinPapierArray = new ArrayList<>();
-
-
-    private void addComputerChoicesToArray() {
-        SchereSteinPapierArray.add("Schere ✂️");
-        SchereSteinPapierArray.add("Stein \uD83E\uDEA8");
-        SchereSteinPapierArray.add("Papier \uD83D\uDCDD");
-    }
-
-    private void automaticComputerChoice() {
-        this.addComputerChoicesToArray();
-        Collections.shuffle(SchereSteinPapierArray);
-
-        computerOutput = SchereSteinPapierArray.get(0);
-
-      System.out.println("Der"+TerminalColorStrings.ANSI_GREEN+" Computer"+TerminalColorStrings.ANSI_RESET+" hat "+computerOutput+ " ausgewählt.\n");
-
+    public void run() {
+        userChoice();
+        automaticComputerChoice();
+        determineWinner();
     }
 
     private void userChoice() {
@@ -39,6 +26,28 @@ public class SchereSteinPapier {
         }
         System.out.println("Der "+TerminalColorStrings.ANSI_BLUE+"User"+TerminalColorStrings.ANSI_RESET+" hat "+userInput+ " ausgewählt.\n");
     }
+
+    private void automaticComputerChoice() {
+        this.addComputerChoicesToArray();
+        Collections.shuffle(SchereSteinPapierArray);
+
+        computerOutput = SchereSteinPapierArray.get(0);
+
+        System.out.println("Der"+TerminalColorStrings.ANSI_GREEN+" Computer"+TerminalColorStrings.ANSI_RESET+" hat "+computerOutput+ " ausgewählt.\n");
+
+    }
+
+    private void addComputerChoicesToArray() {
+        SchereSteinPapierArray.add("Schere ✂️");
+        SchereSteinPapierArray.add("Stein \uD83E\uDEA8");
+        SchereSteinPapierArray.add("Papier \uD83D\uDCDD");
+    }
+
+    private void determineWinner() {
+        System.out.println(TerminalColorStrings.ANSI_BLUE+"User:"+TerminalColorStrings.ANSI_RESET+" ("+userInput+") vs"+TerminalColorStrings.ANSI_GREEN+" Computer: "+TerminalColorStrings.ANSI_RESET+ "("+ computerOutput+")\n");
+        this.winLogic();
+    }
+
 
     public void winLogic() {
       //          Schere verliert gegen stein, Schere gewinnt gegen Papier
@@ -65,16 +74,5 @@ public class SchereSteinPapier {
         } else {
             System.out.println(TerminalColorStrings.ANSI_YELLOW+"(GEWINNER) "+ TerminalColorStrings.ANSI_GREEN+"Computer:"+TerminalColorStrings.ANSI_RESET+ "("+computerOutput+")"+" hat gewonnen.");
         }
-    }
-
-    private void determineWinner() {
-    System.out.println(TerminalColorStrings.ANSI_BLUE+"User:"+TerminalColorStrings.ANSI_RESET+" ("+userInput+") vs"+TerminalColorStrings.ANSI_GREEN+" Computer: "+TerminalColorStrings.ANSI_RESET+ "("+ computerOutput+")\n");
-    this.winLogic();
-    }
-
-    public void run() {
-        userChoice();
-        automaticComputerChoice();
-        determineWinner();
     }
 }
